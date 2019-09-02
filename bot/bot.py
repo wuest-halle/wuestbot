fc17a01e62285c7ce8069fce7593bb96477e1158
Robert Schulze <robert_schulze@posteo.de>
2019-09-02 18:18:45 +0200

    added app imports along __init__ and .flaskenv, works as flask app now

--- bot/bot.py
+++ bot/bot.py
@@ -3,9 +3,27 @@
 
 """ This is the main application module with all the logic in it """
 
+import time
+import json
+import dotenv
+import os
+
+from modules import types
+from app import app
+
+"""
+loads all environment vars from the .env file and makes them available
+"""
+class Config:
+    dotenv.load_dotenv()
+
+    API_TOKEN = os.getenv("API_TOKEN")
+
 
 def main():
-    pass
+    bot = types.Telebot(Config.API_TOKEN)
+    bot.get_updates()
+
 
 if __name__ == "__main__":
     main()
