117ea240120da8deac42b92ca207f5b3dd56c459
Robert Schulze <robert_schulze@posteo.de>
2019-10-27 21:16:04 +0100

    changed formatting, removed to do-comments

--- bot/database/db-create.py
+++ bot/database/db-create.py
@@ -7,32 +7,43 @@ later reference and if the re-creation of the db should be necessary.
 For further understanding see corresponding UML diagram.
 """
 
-""" to do:
-- error management
-- introduce foreign keys to playsAt table
-"""
-
 import sqlite3
 
 conn = sqlite3.connect('data.sqlite')
 curs = conn.cursor()
 
 curs.execute("""
-    create table Users (uID integer primary key, uName text)
+    create table Users (
+    uID integer primary key, 
+    uName text)
     """)
 
 curs.execute("""
-    create table Events (eID integer primary key, eName text, date text, 
-    time text, desc text, admission text, ePicID text)
+    create table Events (
+    eID integer primary key, 
+    eName text, 
+    date text, 
+    time text, 
+    desc text, 
+    admission text, 
+    ePicID text)
     """)
 
 curs.execute("""
-    create table Artists (aName text primary key, website text, soundcloud text, 
-    bandcamp text, bio text, aPicID text)
+    create table Artists (
+    aName text primary key, 
+    website text, 
+    soundcloud text, 
+    bandcamp text, 
+    bio text, 
+    aPicID text)
     """)
 
 curs.execute("""
-    create table playsAt (aName text, eID integer, foreign key (aName, eID) 
-    references Artists (aName), Events (eID)
+    create table playsAt (
+    aName text, 
+    eID integer, 
+    foreign key (aName) references Artists (aName), 
+    foreign key (eID) references Events (eID)
     )
     """)
