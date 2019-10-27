7ad4e237c9ea7985fed9e8bd2379a5e279ef803b
Robert Schulze <robert_schulze@posteo.de>
2019-10-27 10:37:37 +0100

    fixed syntax errors in column creation

--- bot/database/db-create.py
+++ bot/database/db-create.py
@@ -18,15 +18,15 @@ conn = sqlite3.connect('data.sqlite')
 curs = conn.cursor()
 
 curs.execute("""
-    create table Users (integer uID primary key, text name)
+    create table Users (uID integer primary key, name text)
     """)
 
 curs.execute("""
-    create table Events (integer eID primary key, text name, text date, 
-    text time, text desc, text admission, text picID)
+    create table Events (eID integer primary key, name text, date text, 
+    time text, desc text, admission text, picID text)
     """)
 
 curs.execute("""
-    create table Artists (text name primary key, text website, text soundcloud, 
-    text bandcamp, text bio, text picID)
+    create table Artists (name text primary key, website text, soundcloud text, 
+    bandcamp text, bio text, picID text)
     """)
