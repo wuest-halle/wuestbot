8b11c793cf4595f6cddfd27c84c1ae0f7925e824
Robert Schulze <robert_schulze@posteo.de>
2019-11-03 18:26:37 +0100

    fixed SQL string in is_event()

--- bot/app/database/db_objects.py
+++ bot/app/database/db_objects.py
@@ -70,7 +70,7 @@ class Event:
         conn = sqlite3.connect(DB_NAME)
         curs = conn.cursor()
 
-        curs.execute("""select from Artists where aName=?""", (self.name, ))
+        curs.execute("""select * from Events where eName=? and date=?""", (self.name, self.date))
         search = curs.fetchone()
         
         curs.close()
