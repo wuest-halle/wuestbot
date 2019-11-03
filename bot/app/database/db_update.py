fe5c75ecd994c5cb0ea5a19348e0028cce68a35c
Robert Schulze <robert_schulze@posteo.de>
2019-11-03 14:47:26 +0100

    formatting

--- bot/app/database/db_update.py
+++ bot/app/database/db_update.py
@@ -17,39 +17,41 @@ from db_objects import Artist, Event, PlaysAt
 
 def new_event():
 
-        e_name = input("Enter Event-name: ")
-        date = input("Enter date (format DD.MM.YYYY): ")
-        time = input("Enter event time: ")
-        admission = input("Enter admission: ")
-        desc = input("Enter description: ")
-        loca = input("Enter location name: ")
-        e_pic_id = input("Enter the PicID: ")
-
-        event = Event(e_name, date, time, admission, desc, loca, e_pic_id)
-        event.insert_event()
+    e_name = input("Enter Event-name: ")
+    date = input("Enter date (format DD.MM.YYYY): ")
+    time = input("Enter event time: ")
+    admission = input("Enter admission: ")
+    desc = input("Enter description: ")
+    loca = input("Enter location name: ")
+    e_pic_id = input("Enter the PicID: ")
+    
+    event = Event(e_name, date, time, admission, desc, loca, e_pic_id)
+    event.insert_event()
 
 def new_artist():
 
-        a_name = input("Enter the Artist's name: ")
-        webs = input("Enter Website: ")
-        soundc = input("Enter Soundcloud account: ")
-        bandc = input("Enter Bandcamp Profile: ")
-        bio = input("Enter Bio: ")
-        a_pic_id = input("Enter the PicID: ")
-
-        artist = Artist(a_name, webs, soundc, bandc, bio, a_pic_id)
-        artist.insert_artist()
+    a_name = input("Enter the Artist's name: ")
+    webs = input("Enter Website: ")
+    soundc = input("Enter Soundcloud account: ")
+    bandc = input("Enter Bandcamp Profile: ")
+    bio = input("Enter Bio: ")
+    a_pic_id = input("Enter the PicID: ")
+    
+    artist = Artist(a_name, webs, soundc, bandc, bio, a_pic_id)
+    artist.insert_artist()
 
 def plays_at_relation():
 
-        a_name = input("Enter the Artist's name: ")
-        e_name = input("Enter the Event's name: ")
-        date = input("Enter the Event's date (format DD.MM.YYYY): ")
+    a_name = input("Enter the Artist's name: ")
+    e_name = input("Enter the Event's name: ")
+    date = input("Enter the Event's date (format DD.MM.YYYY): ")
+    
+    plays_at = PlaysAt(a_name, e_name, date)
+    PlaysAt.check_for_artist()
+    PlaysAt.check_for_event()
+    PlaysAt.insert_plays_at()
 
-        plays_at = PlaysAt(a_name, e_name, date)
-        PlaysAt.check_for_artist()
-        PlaysAt.check_for_event()
-        PlaysAt.insert_plays_at()
+def call_artist()
 
 if __name__ == "__main__":
     
