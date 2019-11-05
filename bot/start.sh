8d7ef9b5cb6324a44d448ad9bed76340fc99bd7b
Robert Schulze <robert_schulze@posteo.de>
2019-11-05 16:03:44 +0100

    added start.sh as gunicorn startup script

---
+++ bot/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+# startup script for wuestbot on gunicorn
+
+# Project Name
+NAME="wuestbot"
+
+# Host to listen on
+HOST=127.0.0.1
+
+# Port to listen on
+PORT=8443
+
+# Number of gunicorn Workers
+NUM_WORKERS=2
+
+# Databank directory
+DB_DIR=app/database
+
+# create database
+source $DB_DIR/db_create.py
+
+# startup server
+exec gunicorn $NAME:app \
+    -b $HOST:$PORT
+    -w $NUM_WORKERS
