db4383cdd9fe89393aed2257326aba207bb66234
Alexander Knipping <alexander.knipping@noris.de>
2019-11-18 13:05:01 +0100

    Add test scaffold for testing User

---
+++ bot/tests/test_user.py
@@ -0,0 +1,8 @@
+from app.database.db_objects import User, DB_NAME
+from app.database.db_create import create_db
+
+def setup_function():
+    """Setting up the database."""
+
+def teardown_function():
+    """Tearing down the database."""
