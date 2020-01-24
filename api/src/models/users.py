7472cb45be5a6e5113b5ef5b44d1614a3c4aee5b
Robert Schulze <robert_schulze@posteo.de>
2020-01-24 18:22:55 +0100

    added docstrings

--- api/src/models/users.py
+++ api/src/models/users.py
@@ -1,6 +1,13 @@
 from db import db
 
 class Users(db.Model):
+    """Class representing a single instance of an user record
+
+    Arguments:
+        id (int) = the user's unique ID, equivalent to Telegram's ID
+        first_name (str) = user's first name
+        is_bot (bool) = whether the user is a chatbot or not
+    """
 
     __tablename__ = 'Users'
 
@@ -9,6 +16,7 @@ class Users(db.Model):
     is_bot = db.Column(db.Boolean, nullable=False)
 
     def save(self):
+    """Saves a single user to the database"""
 
         try:
             db.session.add(self)
@@ -18,18 +26,47 @@ class Users(db.Model):
             
     @classmethod
     def retrieve_single_user(cls,id):
-        
+    """Classmethod, retrieves a single user from the DB
+
+    Arguments:
+        id: id of the user in question
+
+    Returns:
+        single user object
+    """
+
         return cls.query.get(id)
     
     @classmethod
     def delete_user(cls, id): 
+    """Classmethod, deletes single user record from the DB
+
+    Arguments:
+        id: id of the user in question
+
+    Returns:
+        nothing
+    """
         
         with self.retrieve_single_user(id) as user:
             db.session.delete(user)
             db.session.commit()
 
     @classmethod
-    def update_user(cls, id, name, is_bot):
+    def update_user(cls, id, first_name, is_bot):
+    """Classmethod, updates attributes of a single user's record
+
+    Works with a dirty hack right now: It deletes the record of the user with 
+    the provided ID and then saves a new record with provided parameters
+
+    Arguments:
+        * id: id of the user which should be updated
+        * first_name: new name of the user
+        * is_bot: new status of the user as bot
+    
+    Returns:
+        nothing
+    """
 
         with self.retrieve_single_user(id) as former_user:
             self.delete_user(former_user.id)
@@ -38,5 +75,13 @@ class Users(db.Model):
 
     @staticmethod
     def retrieve_all_users():
+        """Static method, retrieves all users from the DB
+
+        Arguments:
+            none
+        
+        Returns:
+            List of User objects
+        """
 
         return Users.query.get().all()
