8e4bb6f96818d62895e62dec80400c63de7c5f0f
Robert Schulze <robert_schulze@posteo.de>
2020-01-26 18:29:00 +0100

    added to do

---
+++ api/src/tests/test_db.py
@@ -0,0 +1,20 @@
+"""Provides classes to test different DB functionalities
+
+TO DO:
+    * test for User table (empty)
+    * test creation and insertion of User object
+    * test update of User object
+    * test list return of all Users
+    * test deletion of User record
+"""
+
+import pytest
+
+from app import app
+from models.users import User
+
+class TestUser():
+
+
+
+
