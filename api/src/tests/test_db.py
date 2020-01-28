6e3988c4b314e9d23d745af00b3132c17bbcc617
Robert Schulze <robert_schulze@posteo.de>
2020-01-28 16:43:23 +0100

    removed context pushing in functions since ctx is now pushed in app fixture

--- api/src/tests/test_db.py
+++ api/src/tests/test_db.py
@@ -5,7 +5,6 @@ TO DO:
 
 import pytest
 
-from app import app
 from db import db
 from models.users import User
 
@@ -21,39 +20,35 @@ class TestUser():
 
         test_user = User(id=1, name="test_user", is_bot=False)
         assert test_user, "Creation of test user Object failed"
-        with app.app_context():
-            test_user.save()
+       
+        test_user.save()
     
     def test_get(client):
         """Tests, if a single user record can be retrieved"""
         
-        with app.app_context():
-            assert User.get(1), "Retrieval of user record failed"
+        assert User.get(1), "Retrieval of user record failed"
 
     def test_update(client):
         """Tests, if a single User record can be updated"""
         id = 1
         name = "user_test"
         is_bot = True
-
-        with app.app_context():
-            User.update(id, name, is_bot)
-            test_user = User.get(1)
+  
+        User.update(id, name, is_bot)
+        test_user = User.get(1)
 
         assert test_user.name == name, "Updating user's name failed"
         assert test_user.is_bot == is_bot, "updating users is_bot attribute failed"
 
     def test_get_all(client):
         """Tests, if all user records can be retrieved in a list"""
-
-        with app.app_context():
-            test_users = User.get_all()
+       
+        test_users = User.get_all()
         
         assert type(test_users) is list, "Return data type for getting all Users is not a list"
 
     def test_delete(client):
         """Tests, if a single user record can be deleted"""
 
-        with app.app_context():
-            User.delete(1)
-            assert User.get(1) is None, "Deletion of user record failed"
+        User.delete(1)
+        assert User.get(1) is None, "Deletion of user record failed"
