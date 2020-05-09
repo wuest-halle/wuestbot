54d19ddc3d96eeb60173dd4dc90c442ca2cda7d2
Robert Schulze <robert_schulze@posteo.de>
2020-05-09 09:53:30 +0200

    documentation, code fragments clean up

--- api/src/tests/test_db.py
+++ api/src/tests/test_db.py
@@ -75,8 +75,8 @@ def test_create_multiple(session):
             assert ins_user["is_bot"] == retr_user.is_bot, f"bot status does not match for {ins_user}, {retr_user}"
     
 # test cases for the sad path
-# @pytest.mark.parametrize("input", [pytest.param(1, marks=pytest.mark.xfail)])
 def test_invalid_input(session):
+    """ test for faulty input, such as lacking arguments, wrong type of arguments """
     
     user_1 = User(id=1)
     user_1.save()
@@ -91,6 +91,8 @@ def test_invalid_input(session):
 
 
 def test_user_exists(session):
+    """ test for integrity constraints (double input of unique ID) """
+
     # insert first user
     user_1 = User(id=111, name="Test User", is_bot=False) 
     
@@ -98,51 +100,3 @@ def test_user_exists(session):
     # should throw an integrity error
     user_2 = User(id=111, name="Test User", is_bot=False)
     assert sqlalchemy.exc.IntegrityError 
-
-#class TestUser():
-#
-#    def test_table_exists(database):
-#        """Tests, if table Users is existing"""
-#
-#        print(db.engine)
-#
-#        assert "Users" in db.metadata.tables, "Table 'Users' does not exist"
-#
-#    def test_create(database):
-#        """Tests, if User object can be created and saved"""
-#
-#        test_user = User(id=1, name="test_user", is_bot=False)
-#        assert test_user, "Creation of test user Object failed"
-#       
-#        test_user.save()
-#    
-#    def test_get(database):
-#        """Tests, if a single user record can be retrieved"""
-#        
-#        assert User.get(1), "Retrieval of user record failed"
-#
-#    def test_update(database):
-#        """Tests, if a single User record can be updated"""
-#        id = 1
-#        name = "user_test"
-#        is_bot = True
-#  
-#        User.update(id, name, is_bot)
-#        test_user = User.get(1)
-#
-#        assert test_user.name == name, "Updating user's name failed"
-#        assert test_user.is_bot == is_bot, "updating users is_bot attribute failed"
-#
-#    def test_get_all(database):
-#        """Tests, if all user records can be retrieved in a list"""
-#       
-#        test_users = User.get_all()
-#        
-#        assert type(test_users) is list, "Return data type for getting all Users is not a list"
-#
-#    def test_delete(database):
-#        """Tests, if a single user record can be deleted"""
-#
-#        User.delete(1)
-#        assert User.get(1) is None, "Deletion of user record failed"
-#
