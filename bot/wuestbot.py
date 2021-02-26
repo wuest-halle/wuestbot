8fcce42429fc7de8db615790ecc8abc0169bdd42
wuest server admin <wuestwuest@gmx.de>
2021-02-26 17:16:26 +0100

    removed dangling code bits from example

--- bot/wuestbot.py
+++ bot/wuestbot.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 import logging
 import time
@@ -18,9 +16,6 @@ API_TOKEN = os.getenv('API_TOKEN', '')
 
 HOST = os.getenv('HOST', '')
 PORT = os.getenv('PORT', '8443')
-# WEBHOOK_LISTEN = os.getenv('LISTEN_ON', '')
-# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
 WEBHOOK_URL = f"https://{HOST}:{PORT}/bot{API_TOKEN}"
 
@@ -29,13 +24,6 @@ logging.basicConfig(filename='log.txt', encoding='utf-8', level=logging.DEBUG)
 bot = telebot.TeleBot(API_TOKEN)
 
 app = flask.Flask(__name__)
-
-
-# Empty webserver index, return nothing, just http 200
-#@app.route('/', methods=['GET', 'HEAD'])
-#def index():
-#    return ''
-
 
 # Process webhook calls
 @app.route(f'/bot', methods=['POST'])
@@ -58,17 +46,3 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
-
-
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-# bot.remove_webhook()
-
-# time.sleep(1)
-# 
-# # Set webhook
-# bot.set_webhook(url=WEBHOOK_URL)
-
-# Start flask server
-# app.run(host=WEBHOOK_LISTEN,
-#        port=WEBHOOK_PORT,
-#        debug=True)
